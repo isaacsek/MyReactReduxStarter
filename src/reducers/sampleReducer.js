4ce46a20fa4980d63345977cbfbd65295bf9c276
isek27 <isaacsek@uw.edu>
2017-07-29 21:17:11 -0700

    init

--- src/reducers/sampleReducer.js
+++ src/reducers/sampleReducer.js
@@ -1,9 +1,8 @@
-import {FETCH_USER} from "../actions/types";
+import { FETCH_DATA } from "../actions/types";
 
 export default function(state = {}, action) {
-  console.log(action);
     switch (action.type) {
-        case FETCH_USER:
+        case FETCH_DATA:
           return {...state, data: action.payload}
         default:
             return state;
